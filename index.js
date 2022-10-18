4ae1c9e2ec92258b307cccf5fd4b403458a9cab8
andrzul <aineken93@gmail.com>
2022-10-18 17:53:14 +0200

    update server-app version8

--- index.js
+++ index.js
@@ -19,10 +19,12 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5005;
-const CONNECTION_URL = process.env.CONNECTION_URL;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
